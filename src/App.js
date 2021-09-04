e7b51f54b6d0ed06f7232af1153497b668cdab71
Paudelsrijana <paudel.srijana2055@gmail.com>
2021-09-04 10:19:55 +0545

    frst

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import NotesItem from "./NotesItem";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notesTitle: "",
+      notesDetail: "",
+      notesDate: "",
+      notes: window.localStorage.getItem("items")
+        ? JSON.parse(window.localStorage.getItem("items"))
+        : [
+            {
+              notesTitleText: "hello",
+              notesDetailText: "hy",
+              notesDate: "12/12/1234",
+            },
+          ],
+    };
+  }
+  handleTitleInputChange = (event) => {
+    const value = event.target.value;
+    this.setState({
+      notesTitle: value,
+    });
+  };
+  handleDetailInputChange = (event) => {
+    const value = event.target.value;
+    this.setState({
+      notesDetail: value,
+    });
+  };
+  handleDateInputChange = (event) => {
+    const date = new Date();
+    this.setState({
+      notesDate: date.toLocaleDateString(),
+    });
+  };
+  handleSaveClick = (e) => {
+    e.preventDefault();
+    if (this.state.notesTitle !== "" && this.state.notesDetail !== "") {
+      const notesTitle = this.state.notesTitle;
+      const notesDetail = this.state.notesDetail;
+      const notesDate = this.state.notesDate;
+      const note = {
+        notesTitleText: notesTitle,
+        notesDetailText: notesDetail,
+        notesDateInput: notesDate,
+      };
+      const notesCloned = this.state.notes.slice();
+      notesCloned.push(note);
+      window.localStorage.setItem("items", JSON.stringify(notesCloned));
+      this.setState({
+        notes: notesCloned,
+        notesTitle: "",
+        notesDetail: "",
+        notesDate: "",
+      });
+    }
+    console.log("hey");
+  };
+  render() {
+    return (
+      <div className="container">
+        <form>
+          <div className="header">
+            <div className="logo col-lg-4">
+              <img src="logo.png" alt="logo" />
+            </div>
+            <h4 className="col-lg-4">Add Your New Notes Here: </h4>
+            <button className="add-click">
+              <i className="fa fa-plus-square add-notes col-lg-4" />
+            </button>
+          </div>
+          <div className="note-lists">
+            {this.state.notes.map((note, i) => {
+              return (
+                <NotesItem
+                  key={i}
+                  notesTitle={note.notesTitleText}
+                  notesContent={note.notesDetailText}
+                  notesDate={note.notesDateInput}
+                  onNotesTitleInput={this.handleTitleInputChange}
+                  onNotesDetailInput={this.handleDetailInputChange}
+                  onNotesDate={this.handleDateInputChange}
+                  onSaveClick={this.handleSaveClick}
+                />
+              );
+            })}
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/NotesItem.js
@@ -0,0 +1,35 @@
+const NotesItem = (props) => {
+  return (
+    <div className="notes new">
+      <textarea
+        rows="2"
+        cols="10"
+        placeholder=" Note Title"
+        value={props.notesTitle}
+        onChange={props.onNotesTitleInput}
+      />
+      <div className="edit-del-btn">
+        <button>✍️</button>
+        <button>
+          <i className="fa fa-trash" />
+        </button>
+      </div>
+
+      <textarea
+        rows="8"
+        cols="10"
+        placeholder="Note Content"
+        value={props.notesContent}
+        onChange={props.onNotesDetailInput}
+      />
+      <div className="note-footer">
+        <small onChange={props.onNotesDate}>{props.notesDate}</small>
+      </div>
+      <button className="save" onClick={props.onSaveClick}>
+        Save Note
+      </button>
+    </div>
+  );
+};
+
+export default NotesItem;
