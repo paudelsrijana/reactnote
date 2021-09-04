f6fd9fe8ec5ef35011794b448dafec0735c8385e
Paudelsrijana <paudel.srijana2055@gmail.com>
2021-09-04 22:56:34 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ class App extends Component {
       notesTitle: "",
       notesDetail: "",
       notesDate: "",
+      searchInput: "",
       notes: window.localStorage.getItem("items")
         ? JSON.parse(window.localStorage.getItem("items"))
         : [],
+      showNoteAddInput: false,
     };
   }
   handleTitleInputChange = (event) => {
@@ -20,9 +22,11 @@ class App extends Component {
   };
   handleDetailInputChange = (event) => {
     const value = event.target.value;
-    this.setState({
-      notesDetail: value,
-    });
+    if (value.length <= 300) {
+      this.setState({
+        notesDetail: value,
+      });
+    }
   };
 
   handleSaveClick = (e) => {
@@ -35,19 +39,74 @@ class App extends Component {
         notesTitleText: notesTitle,
         notesDetailText: notesDetail,
         notesDateInput: notesDate.toLocaleDateString(),
+        editMode: false,
       };
       const notesCloned = this.state.notes.slice();
       notesCloned.push(note);
-      window.localStorage.setItem("items", JSON.stringify(notesCloned));
       this.setState({
         notes: notesCloned,
         notesTitle: "",
         notesDetail: "",
         notesDate: "",
+        showNoteAddInput: false,
       });
     }
   };
+  handleRemoveNotes = (index) => {
+    const notesCloned = this.state.notes.slice();
+    notesCloned.splice(index, 1);
+    this.setState({
+      notes: notesCloned,
+    });
+    console.log("hey");
+    window.localStorage.setItem("items", JSON.stringify(notesCloned));
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.notes !== this.state.notes) {
+      window.localStorage.setItem("items", JSON.stringify(this.state.notes));
+    }
+  }
+
+  toggleAddInput = () => {
+    this.setState((state) => ({ showNoteAddInput: !state.showNoteAddInput }));
+  };
+  handleSearchInputChange = (event) => {
+    const value = event.target.value;
+    this.setState({
+      searchInput: value,
+    });
+  };
+  searchnotes = () => {
+    return this.state.notes.filter((note) => {
+      return note.notesTitleText
+        .toLowerCase()
+        .includes(this.state.searchInput.toLowerCase());
+    });
+  };
+  handleEditMode = (index) => {
+    const notesCloned = this.state.notes.slice();
+    const prevEditMode = notesCloned[index].editMode;
+    notesCloned[index].editMode = !prevEditMode;
+
+    this.setState({
+      notes: notesCloned,
+    });
+    console.log("hey");
+  };
+
+  handleEditNoteChange = (event, index) => {
+    const notesCloned = this.state.notes.slice();
+    const title = event.target.value;
+    const detail = event.target.value;
+    notesCloned[index].notesTitleText = title;
+    notesCloned[index].notesDetailText = detail;
+    this.setState({
+      notes: notesCloned,
+    });
+  };
   render() {
+    const filtered = this.searchnotes();
     return (
       <div className="container">
         <form>
@@ -56,21 +115,55 @@ class App extends Component {
               <img src="logo.png" alt="logo" />
             </div>
             <h4 className="col-lg-4">Add Your New Notes Here: </h4>
-            <button className="add-click">
+            <button
+              type="button"
+              className="add-click"
+              onClick={this.toggleAddInput}
+            >
               <i className="fa fa-plus-square add-notes col-lg-4" />
             </button>
           </div>
+          <div className="search-div">
+            <input
+              type="text"
+              className="form-control "
+              placeholder="Search your todos...."
+              id="searchInput"
+              onChange={this.handleSearchInputChange}
+              value={this.state.searchInput}
+            />
+          </div>
+          {this.state.showNoteAddInput ? (
+            <NotesItem
+              notesTitle={this.state.notesTitle}
+              notesContent={this.state.notesDetail}
+              notesDate={this.state.notesDate}
+              onNotesTitleInput={this.handleTitleInputChange}
+              onNotesDetailInput={this.handleDetailInputChange}
+              onSaveClick={this.handleSaveClick}
+            />
+          ) : null}
           <div className="note-lists">
-            {this.state.notes.map((note, i) => {
+            {filtered.map((note, i) => {
               return (
                 <NotesItem
                   key={i}
                   notesTitle={note.notesTitleText}
                   notesContent={note.notesDetailText}
                   notesDate={note.notesDateInput}
-                  onNotesTitleInput={this.handleTitleInputChange}
-                  onNotesDetailInput={this.handleDetailInputChange}
-                  onSaveClick={this.handleSaveClick}
+                  // onNotesTitleInput={this.handleTitleInputChange}
+                  // onNotesDetailInput={this.handleDetailInputChange}
+                  // onSaveClick={this.handleSaveClick}
+                  onDeleteClick={() => {
+                    this.handleRemoveNotes(i);
+                  }}
+                  editMode={note.editMode}
+                  onEditMode={() => {
+                    this.handleEditMode(i);
+                  }}
+                  onEditNoteChange={(e) => {
+                    this.handleEditNoteChange(e, i);
+                  }}
                 />
               );
             })}
--- src/NotesItem.js
+++ src/NotesItem.js
@@ -2,22 +2,20 @@ const NotesItem = (props) => {
   return (
     <div className="notes">
       <textarea
-        rows="2"
-        cols="10"
         placeholder=" Note Title"
         value={props.notesTitle}
         onChange={props.onNotesTitleInput}
       />
       <div className="edit-del-btn">
-        <button>✍️</button>
-        <button>
+        <button onChange={props.onEditNoteChange} onClick={props.onEditMode}>
+          ✍️
+        </button>
+        <button onClick={props.onDeleteClick}>
           <i className="fa fa-trash" />
         </button>
       </div>
 
       <textarea
-        rows="8"
-        cols="10"
         placeholder="Note Content"
         value={props.notesContent}
         onChange={props.onNotesDetailInput}
@@ -25,6 +23,7 @@ const NotesItem = (props) => {
       <div className="note-footer">
         <small>{props.notesDate} </small>
       </div>
+      <small>{300 - props.notesContent.length}</small>
       <button className="save" onClick={props.onSaveClick}>
         Save Note
       </button>
